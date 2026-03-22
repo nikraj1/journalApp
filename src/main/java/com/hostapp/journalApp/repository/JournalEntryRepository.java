43d44faa61ddd481a060619b403cdb7be5b85c76
Nikhil <rajnikhil2979@gmail.com>
2026-03-22 23:37:09 +0530

    Initial commit

---
+++ src/main/java/com/hostapp/journalApp/repository/JournalEntryRepository.java
@@ -0,0 +1,8 @@
+package com.hostapp.journalApp.repository;
+import com.hostapp.journalApp.entity.JournalEntry;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface JournalEntryRepository extends MongoRepository<JournalEntry, ObjectId> {
+
+}
